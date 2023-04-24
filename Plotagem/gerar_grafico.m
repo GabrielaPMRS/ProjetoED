e9fe248c5f656bf7be00857d532d566dc77fd8cf
GabrielaPMRS <gabrielasoares.ec@gmail.com>
2023-04-23 21:02:31 -0300

    separar graficos plotagem

--- Plotagem/gerar_grafico.m
+++ Plotagem/gerar_grafico.m
@@ -1,9 +1,18 @@
 filename = 'saida.txt';
 m = csvread(filename);
 
-%sorted = sortrows(m, 1);
-%disp(sorted);
+sorted = sortrows(m, 2);
+disp(sorted);
+disp(m);
 %plot(sorted(:,1), sorted(:, 2), 'mo', sorted(:,1), sorted(:,3), 'bo');
+%plot(m(:,1), m(:, 2), 'm', m(:,1), m(:, 3), 'b');
+
+fig1 = figure();
+plot(m(:,1), m(:, 2), 'b.');
+
+fig2 = figure();
+plot(m(:,1), m(:, 3), 'r.');
+
+fig3 = figure();
 plot(m(:,1), m(:, 2), 'm', m(:,1), m(:, 3), 'b');
-%plot(m(:,1), m(:, 3), 'b');
-%plot(m(:, 1), m(:, 2), 'r');
+
